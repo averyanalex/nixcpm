8a5027e6d70bbc945909996ba1637bc05805dac4
AveryanAlex <alex@averyan.ru>
2023-01-27 08:42:45 +0300

    OnBoard settings

--- profiles/onboard.nix
+++ profiles/onboard.nix
@@ -3,4 +3,18 @@
   home-manager.users.user = {
     home.packages = with pkgs; [ onboard ];
   };
+
+  home-manager.users.user.dconf.settings = {
+    "org/onboard/window/landscape" = {
+      height = 420;
+      width = 1400;
+    };
+    "org/onboard/window" = {
+      docking-enabled = false;
+      transparency = 10;
+    };
+    "org/onboard" = {
+      layout = "${pkgs.onboard}/share/onboard/layouts/Full Keyboard.onboard";
+    };
+  };
 }
